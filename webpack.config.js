14bcbd4ed0c396ad8b0bbf4fd1756150cd3ec3b0
Chandana Rajarapu <chandana@Chandanas-MacBook-Air.local>
2025-10-31 15:15:15 +0530

    Fix runtime config for Vercel functions

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const CustomFunctionsMetadataPlugin = require("custom-functions-metadata-plugin"
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const urlDev = "https://localhost:3000/";
-const urlProd = "https://www.contoso.com/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
+const urlProd = "https://excel-openai-proxy-ekat.vercel.app/"; // Production deployment URL
 
 /* global require, module, process */
 
